334e093c60d8d906d2b8bbb5bc6324e5be0bc932
Felix Boehme <git@felixboehme.dev>
2024-06-10 11:01:56 +0200

    test: increasing beyond the maximum capacity

--- src/test/java/felix/store/EnergyStoreServiceTest.java
+++ src/test/java/felix/store/EnergyStoreServiceTest.java
@@ -1,9 +1,12 @@
 package felix.store;
 
+import felix.network.NetworkRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import felix.network.NetworkRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
@@ -14,18 +17,44 @@ import static org.mockito.Mockito.*;
 public class EnergyStoreServiceTest {
 
     @Test
+    public void throwExceptionWhenStoreNotFound() {
+        NetworkRepository networkRepository = mock(NetworkRepository.class);
+        EnergyStoreRepository energyStoreRepository = Mockito.mock(EnergyStoreRepository.class);
+        EnergyStoreService energyStoreService = new EnergyStoreService(networkRepository, energyStoreRepository);
+
+        Exception exception = Assertions.assertThrows(EntityNotFoundException.class, () -> energyStoreService.getEnergyStore(0L));
+
+        assertThat(exception.getMessage()).is("Couldn't find Store with ID: 0");
+    }
+
+    @Test
     public void throwExceptionWhenDecreasingCapacityBelowZero() {
         NetworkRepository networkRepository = mock(NetworkRepository.class);
         EnergyStoreRepository energyStoreRepository = Mockito.mock(EnergyStoreRepository.class);
-        EnergyStore energyStore = new EnergyStore();
-        when(energyStoreRepository.findById(0L)).thenReturn(Optional.ofNullable(energyStore));
+        EnergyStore energyStore = new EnergyStore(EnergyStoreType.SOLAR, 0F, 0F, "Europahaus", null);
+        when(energyStoreRepository.findByIdActive(0L)).thenReturn(Optional.of(energyStore));
 
         EnergyStoreService energyStoreService = new EnergyStoreService(networkRepository, energyStoreRepository);
 
-        Exception exception = Assertions.assertThrows(RuntimeException.class, () ->
+        Exception exception = Assertions.assertThrows(ResponseStatusException.class, () ->
                 energyStoreService.updateCurrentCapacity(0L, -500F));
 
-        assertThat(exception.getMessage()).isEqualTo("can't reduce capacity below zero");
+        assertThat(exception.getMessage()).isEqualTo(new ResponseStatusException(HttpStatus.BAD_REQUEST).getMessage());
+        verify(energyStoreRepository, never()).save(any());
+    }
+
+    @Test
+    public void throwExceptionWhenIncreasingAboveMaxCapacity() {
+        NetworkRepository networkRepository = mock(NetworkRepository.class);
+        EnergyStoreRepository energyStoreRepository = Mockito.mock(EnergyStoreRepository.class);
+        EnergyStore energyStore = new EnergyStore(EnergyStoreType.SOLAR, 0F, 0F, "Europahaus", null);
+        when(energyStoreRepository.findByIdActive(0L)).thenReturn(Optional.of(energyStore));
+
+        EnergyStoreService energyStoreService = new EnergyStoreService(networkRepository, energyStoreRepository);
+
+        Exception exception = Assertions.assertThrows(ResponseStatusException.class, () -> energyStoreService.updateCurrentCapacity(0L, 500F));
+
+        assertThat(exception.getMessage()).isEqualTo(new ResponseStatusException(HttpStatus.BAD_REQUEST).getMessage());
         verify(energyStoreRepository, never()).save(any());
     }
 }
